5f705af93eff13f5561790f51548ef94f1a910f9
红血球AE3803 <2544390577@qq.com>
2024-04-14 13:57:04 +0900

    fixed bug

--- inc/func/get_uuid.php
+++ inc/func/get_uuid.php
@@ -3,9 +3,9 @@ function get_uuid($api, $token, $repo, $filename) {
     $url = $api.'/datasets/'.$repo.'/current_repo?access_token='.$token.'&type=0';
     $data = json_decode(get_data($url), true);
     $d = $data["data"][0]["attachments"];
-    foreach ($d as $key => $value) {
-        if (in_array($filename, $value)) {
-            return $value['uuid'];
+    foreach ($d as $item) {
+        if ($item['name'] == $filename) {
+            return array($item['uuid'], $item['size']);
         }
     }
 }
